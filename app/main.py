79bdf558d1015a0e5f5fdab989d5ea8df6b0c9c7
Anandhaganesh A <anandhaganesh210@gmail.com>
2025-12-27 19:40:24 +0530

    initial database connection completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
+from app.models import user
 
 app = FastAPI(title="Job-Applications-Tracker-API")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
